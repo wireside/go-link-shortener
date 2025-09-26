abbf154ef3db827cee54f4da5a1d7e5f187cd6f6
wireside <wiresifed@gmail.com>
2025-09-26 20:00:40 +0300

    feat: created empty link repository that works with gorm

--- internal/link/repository.go
+++ internal/link/repository.go
@@ -1,1 +1,19 @@
 package link
+
+import (
+	"go-adv-demo/pkg/db"
+)
+
+type LinkRepository struct {
+	database *db.Db
+}
+
+func NewLinkRepository(database *db.Db) *LinkRepository {
+	return &LinkRepository{
+		database,
+	}
+}
+
+func (repo LinkRepository) Create(link *Link) *Link {
+	return nil
+}
